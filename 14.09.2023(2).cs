1f1a83dfbf13d5d4841139cf8ee745dc59f9d86d
Ali200th15 <aliismayilth@gmail.com>
2023-09-24 04:03:08 +0400

    HW

---
+++ 14.09.2023(2).cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+Calculator calculator = new ();
+
+Calculator.MathOperation addition = calculator.Add;
+Calculator.MathOperation subtraction = calculator.Subtract;
+Calculator.MathOperation multiplication = calculator.Multiply;
+Calculator.MathOperation division = calculator.Divide;
+
+double num1 = 10;
+double num2 = 5;
+
+Console.WriteLine($"Addition: {num1} + {num2} = {calculator.Calculate(addition, num1, num2)}");
+Console.WriteLine($"Subtraction: {num1} - {num2} = {calculator.Calculate(subtraction, num1, num2)}");
+Console.WriteLine($"Multiplication: {num1} * {num2} = {calculator.Calculate(multiplication, num1, num2)}");
+Console.WriteLine($"Division: {num1} / {num2} = {calculator.Calculate(division, num1, num2)}");
+
+public class Calculator
+{
+    public delegate double MathOperation(double num3, double num4);
+
+    public double Add(double num3, double num4)
+    {
+        return num3 + num4;
+    }
+
+    public double Subtract(double num3, double num4)
+    {
+        return num3 - num4;
+    }
+
+    public double Multiply(double num3, double num4)
+    {
+        return num3 * num4;
+    }
+
+    public double Divide(double num3, double num4)
+    {
+        if (num4 == 0)
+        {
+            Console.WriteLine("Error");
+        }
+        return num3 / num4;
+    }
+
+    public double Calculate(MathOperation operation, double num3, double num4)
+    {
+        return operation(num3, num4);
+    }
+}
---
+++ 14.09.2023.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+BankAccount account1 = new BankAccount("Account 1", 1000.0);
+BankAccount account2 = new BankAccount("Account 2", 500.0);
+
+account1.Register(amount => Console.WriteLine($"Operation on Account 1: {amount}"));
+account2.Register(amount => Console.WriteLine($"Operation on Account 2: {amount}"));
+
+account1.PrintBalance();
+account2.PrintBalance();
+
+account1.Deposit(200.0);
+account2.Deposit(300.0);
+
+account1.Withdraw(50.0);
+account2.Withdraw(100.0);
+
+account1.PrintBalance();
+account2.PrintBalance();
+
+delegate void Operation(double amount);
+
+class BankAccount
+{
+    private double Balance { get; set; }
+    private string Name { get; set; }
+    private Operation operation { get; set; }
+
+    public BankAccount(string name, double balance)
+    {
+        Name = name;
+        Balance = balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"Deposited {amount} into {Name}'s account");
+        operation.Invoke(amount);
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn {amount} from {Name}'s account");
+            operation.Invoke(-amount);
+        }
+        else
+        {
+            Console.WriteLine($"Error: Insufficient funds in {Name}'s account");
+        }
+    }
+
+    public void PrintBalance()
+    {
+        Console.WriteLine($"Balance of {Name}'s account: {Balance}");
+    }
+
+    public void Register(Operation num1)
+    {
+        operation += num1;
+    }
+}
