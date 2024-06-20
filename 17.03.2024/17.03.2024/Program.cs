29f57842d06c3f6f5b6634d7e767910ba0c7548d
Ali200th15 <aliismayilth@gmail.com>
2024-06-21 02:33:24 +0400

    HW

---
+++ 17.03.2024/17.03.2024/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+var sampleTodos = new Todo[] {
+    new(1, "Buy groceries"),
+    new(2, "Finish the project report", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Schedule dentist appointment", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Pay utility bills"),
+    new(5, "Organize the garage", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
