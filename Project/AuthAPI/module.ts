f9b57f0b7e307b5bb612bb8c2ffe2c6bf50d50ee
Ali2007th <ali2007th11@gmail.com>
2025-02-23 09:14:29 +0400

    HW

--- Project/AuthAPI/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModel } from './authGuard.ts/auth.model';
-import { UsersModel } from './users/user.model';
-import { DatabaseContext } from './config/dbContext';
-
-@Module({
-  imports: [AuthModel, UsersModel, DatabaseContext],
-})
-export class AppModule {}
